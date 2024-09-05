3df919f99aa3350dc4fae11961b50a4b0eaa24c3
Li Yushen <lysrain21@163.com>
2024-09-05 16:10:12 +0800

    lys first submit，切换UI为APPLE风格

--- ifupan/main.py
+++ ifupan/main.py
@@ -46,4 +46,4 @@ def download_file(filename):
     return send_file(os.path.join('files', filename), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5002)
--- ifupan/text_analysis.py
+++ ifupan/text_analysis.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from deepseek_v2_langchain import deepseek_analyze
-from ifupan.prompt_fupan import fupan_prompts
+from prompt_fupan import fupan_prompts
 
 def analyze(text, prompt_type):
     task_description = fupan_prompts.get(prompt_type, fupan_prompts['diary'])
